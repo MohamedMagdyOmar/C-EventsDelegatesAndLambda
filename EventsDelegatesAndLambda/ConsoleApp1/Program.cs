ddbbdb8a4e9157f151f54e2f4570b0594cfc7f50
MohamedMagdyOmar <mohamed.magdy.eg@schneider-electric.com>
2017-10-16 11:06:37 +0200

    4-Demo Adding Multiple Delegates to an Invocation List

--- EventsDelegatesAndLambda/ConsoleApp1/Program.cs
+++ EventsDelegatesAndLambda/ConsoleApp1/Program.cs
@@ -15,12 +15,16 @@ namespace ConsoleApp1
         {
             workPerformedHandler del1 = new workPerformedHandler(WorkPerformed1);
             workPerformedHandler del2 = new workPerformedHandler(WorkPerformed2);
-
+            workPerformedHandler del3 = new workPerformedHandler(WorkPerformed3);
             // lets say we had a method, and we do not know what this method should do
             // we need it to be dynamic method
 
+            del1 += del2;
+            del1 += del3;
+
+          
             DoWork(del1);
-            del2(10, WorkType.PlayingFootball);
+            //del2(10, WorkType.PlayingFootball);
             Console.ReadLine();
         }
 
@@ -39,6 +43,11 @@ namespace ConsoleApp1
         public static void WorkPerformed2(int hours, WorkType workType)
         {
             Console.WriteLine("Work Performed 2 called " + hours.ToString());
+        }
+
+        public static void WorkPerformed3(int hours, WorkType workType)
+        {
+            Console.WriteLine("Work Performed 3 called " + hours.ToString());
         }
 
         public enum WorkType
