f06d5b8086e322eb7c3b05b5f9b834ba6933acad
MohamedMagdyOmar <mohamed.magdy.eg@schneider-electric.com>
2017-10-16 11:24:27 +0200

    5-Demo Returning a Value Using a Delegate

--- EventsDelegatesAndLambda/ConsoleApp1/Program.cs
+++ EventsDelegatesAndLambda/ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
     class Program
     {
         // the word "Handler" is typically used when naming the delegates
-        public delegate void workPerformedHandler(int hours, WorkType workType);
+        public delegate int workPerformedHandler(int hours, WorkType workType);
 
         static void Main(string[] args)
         {
@@ -19,35 +19,40 @@ namespace ConsoleApp1
             // lets say we had a method, and we do not know what this method should do
             // we need it to be dynamic method
 
-            del1 += del2;
-            del1 += del3;
+            //del1 += del2;
+            //del1 += del3;
+            del1 += del2 + del3;
 
-          
-            DoWork(del1);
+
+            int finalHours = DoWork(del1);
+            Console.WriteLine(finalHours);
             //del2(10, WorkType.PlayingFootball);
             Console.ReadLine();
         }
 
         // this method become dynamic
         // you pass function to a function
-        public static void DoWork(workPerformedHandler del)
+        public static int DoWork(workPerformedHandler del)
         {
-            del(5, WorkType.Golf);
+            return del(5, WorkType.Golf);
         }
 
-        public static void WorkPerformed1(int hours, WorkType workType)
+        public static int WorkPerformed1(int hours, WorkType workType)
         {
             Console.WriteLine("Work Performed 1 called " + hours.ToString());
+            return hours + 1;
         }
 
-        public static void WorkPerformed2(int hours, WorkType workType)
+        public static int WorkPerformed2(int hours, WorkType workType)
         {
             Console.WriteLine("Work Performed 2 called " + hours.ToString());
+            return hours + 2;
         }
 
-        public static void WorkPerformed3(int hours, WorkType workType)
+        public static int WorkPerformed3(int hours, WorkType workType)
         {
             Console.WriteLine("Work Performed 3 called " + hours.ToString());
+            return hours + 3;
         }
 
         public enum WorkType
