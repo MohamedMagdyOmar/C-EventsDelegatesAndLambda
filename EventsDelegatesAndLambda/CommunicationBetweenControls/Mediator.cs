b91d8aeb61f70a17395ca9a621f8291c30783bcb
MohamedMagdyOmar <mohamed.magdy.eg@schneider-electric.com>
2017-11-22 15:54:42 +0200

    2-Communicating Between Components with Events and Delegates

---
+++ EventsDelegatesAndLambda/CommunicationBetweenControls/Mediator.cs
@@ -0,0 +1,33 @@
+﻿using CommunicationBetweenControls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationBetweenControls
+{
+   public class Mediator
+    {
+        // singleton patter, so we share one instance ("_instance") between all modules
+        private static readonly Mediator _instance = new Mediator();
+        private Mediator()
+        {
+
+        }
+
+        public static Mediator GetInstance()
+        {
+            return _instance;
+        }
+        public event EventHandler<JobChangedEventArgs> JobChanged;
+        public void OnJobChanged(object sender, Job job)
+        {
+            var JobChangeDelegate = JobChanged as EventHandler<JobChangedEventArgs>;
+            if(JobChangeDelegate != null)
+            {
+                JobChangeDelegate(sender, new JobChangedEventArgs { Job = job });
+            }
+        }
+    }
+}
